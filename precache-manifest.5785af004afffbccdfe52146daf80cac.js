118a21d1260a2b341d5cf615c3e0935f094eb41c
Anne LoVerso <anne.loverso@gmail.com>
2020-02-04 15:56:08 -0500

    Updates

---
+++ precache-manifest.5785af004afffbccdfe52146daf80cac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a906492362f72863a8576bb7ee7339a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a012604cb511e2e6fa72",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f06a10935454a5cdddef",
+    "url": "/static/js/2.cc64cbb6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.cc64cbb6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a012604cb511e2e6fa72",
+    "url": "/static/js/main.84ccaa71.chunk.js"
+  },
+  {
+    "revision": "c76914a4db7228077d46",
+    "url": "/static/js/runtime-main.98f0131c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
